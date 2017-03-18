8af1d2c9610bc906e87ccf29cf0518f5f35c9eb4
Ryan Burgett <thesonicdoctor@gmail.com>
2017-03-18 15:13:32 -0400

    attempt at signup

--- psycopg2_test.py
+++ psycopg2_test.py
@@ -43,26 +43,41 @@ def logout():
 		provar = 0
 		print "You have successfully logged out."
 
-def signup(new_uname, new_passwd):
-	myquery = """fdsf sd fdsf"""
+def signup(new_email, new_uname, new_passwd):
+	addUsrPassq = """INSERT INTO "User" (username, password, email) VALUES ({%s}, {%s}, {%s}); """
+	cur.execute(addUsrPassq % (new_uname, new_passwd, new_email))
+
+
+	
 
 if __name__ == '__main__':
 	while(1):
-		if(loginvar==0):
+    	#print "Choose Login or Signup:"
+		choice = raw_input()
+		if(choice == "Login"):
+			if(loginvar==0):
+				print "Please enter your username:"
+				username = raw_input()
+				print "Please enter your password:"
+				password = raw_input()
+				login(username,password)
+
+
+			elif(loginvar==1):
+				print "Would you like to log out? [y/n]"
+				ans = raw_input()
+				if ans=='y':
+					logout()
+				elif ans=='n':
+					print "Would you like to terminate your session? [y/n]"
+					ans = raw_input()
+					if ans=='y':
+						sys.exit()
+		elif(choice == "Signup"):
+    			print "Please enter your email"
+			email = raw_input()
 			print "Please enter your username:"
 			username = raw_input()
 			print "Please enter your password:"
 			password = raw_input()
-			login(username,password)
-
-
-		elif(loginvar==1):
-			print "Would you like to log out? [y/n]"
-			ans = raw_input()
-			if ans=='y':
-				logout()
-			elif ans=='n':
-				print "Would you like to terminate your session? [y/n]"
-				ans = raw_input()
-				if ans=='y':
-					sys.exit()
+			signup(email, username, password)
