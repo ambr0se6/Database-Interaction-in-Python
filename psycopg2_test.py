f6f3cc0b496bd45523f1cedc77b91e5ec0c031ec
grohli <greg.rohlicek@gmail.com>
2017-03-19 14:13:18 -0400

    User and secret owner's wallets updated properly.

--- psycopg2_test.py
+++ psycopg2_test.py
@@ -13,6 +13,9 @@ loginvar = 0
 provar = 0
 current_user = ''
 
+def makeID():
+	return random.randint(0,2147483647)
+
 def login(uname, passwd):
 	global loginvar, provar
 	userpasswordq = """SELECT * FROM "User" WHERE username='{%s}' AND password='{%s}';"""
@@ -62,15 +65,76 @@ def buy_secret(secretID):
 	#		Check to see if by some divine intervention our makeID() is already taken
 	#		Reroll if necessary.
 	#	Update deliver table.
+	myWallets = []
+	owner_dwID = ''
+	secretInfo = []
+	# Our Queries
+	get_secretInfo = """SELECT "price","description" FROM "secretPosting" WHERE "sID"=%d;"""
+	get_dwIDs = """SELECT "dwID" FROM "Owns" WHERE "username"='{%s}';"""
+	get_owner_dwID = """SELECT "dwID" FROM "pSell" WHERE "sID"=%d;"""
+	get_Bitcoin = """SELECT "Bitcoin" FROM "DigitalWallet" WHERE "dwID"=%d; """
+	update_Bitcoin = """UPDATE "DigitalWallet" SET "Bitcoin"=%d WHERE "dwID"=%d;"""
+	
+	cur.execute(get_secretInfo % secretID)
+	try:
+		secretInfo = cur.fetchone()
+	except:
+		print "No such secret"
+
+	print "Price: %d" % (secretInfo[0])
+	print "Description: %s" % (secretInfo[1])
+
+	cur.execute(get_owner_dwID % secretID)
+	try:
+		owner_dwID = cur.fetchone()[0]
+	except:
+		pass
+
+	cur.execute(get_dwIDs % current_user)
+	try:
+		myWallets = cur.fetchall()
+	except:
+		print "Oops! You don't have any digital wallets."
+
+	print "Which wallet would you like to use? "
+	for wallet in myWallets:
+		print wallet
+
+	wallet_to_use = raw_input()
+
+	if wallet_to_use in myWallets:
+		cur.execute(get_Bitcoin % wallet_to_use)
+		btc = 0
+		
+		try:
+			btc = cur.fetchone()[0]
+		except:
+			print "You don't seem to have any money in this wallet."
+
+		if btc > secretInfo[0]:
+			# Remove btc from our wallet
+			new_btc = btc - secretInfo[0]
+			cur.execute(update_Bitcoin % (new_btc, wallet_to_use))
+
+			# Add btc to owner's wallet
+			cur.execute(get_Bitcoin % owner_dwID)
+			try:
+				owner_new_btc = btc + cur.fetchone()[0]
+				cur.execute(update_Bitcoin % (owner_new_btc, owner_dwID))
+			except:
+				pass
+
+			# Update the buysecret table
+			
+			
+		else:
+			print "You don't have enough money in this wallet."
+
 
 def sell_secret(price, encryptInfo, description):
 	#Things to do in this function
 	#	Update pSell, Sellings
 	#	Update the secretPosting table with args
-
-
-def makeID():
-	return random.randint(0,2147483647)
 
 if __name__ == '__main__':
 	while(1):
